31f720c6edf745510ad37b9517eb24b0c03f8ccb
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-25 15:04:19 +0300

    about routes

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-import datetime 
+import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def hello():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+
+@app.route('/')
+def about():
+    return render_template('about.html')
