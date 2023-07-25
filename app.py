7328ad9acbbcbe752720ad5d7eb90b53e06e28c5
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-25 16:08:34 +0300

    index

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import datetime
 from flask import Flask, render_template
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
-@app.route('/')
+@app.route('/about/')
 def about():
     return render_template('about.html')
