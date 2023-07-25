7d6356c12b4051cb5fe8bdc5f198ec414d60f7be
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-25 16:40:26 +0300

    comment in app.py

--- app.py
+++ app.py
@@ -15,9 +15,10 @@ def about():
 
 @app.route('/comments/')
 def comments():
-    comments = [
-        'This is the first comment.',
+    comments = ['This is the first comment.',
                 'This is the second comment.',
                 'This is the third comment.',
                 'This is the fourth comment.'
-    ]
+                ]
+
+    return render_template('comments.html', comments=comments)
